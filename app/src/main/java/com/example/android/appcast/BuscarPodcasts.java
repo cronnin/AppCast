e52cf595ac329e3bb5bf93cf41c033e7c86707bf
cronnin <marcelotononr@gmail.com>
2017-04-26 22:28:15 -0300

    Correções
    Inclusão de botões para voltar para "Reproduzindo"

--- app/src/main/java/com/example/android/appcast/BuscarPodcasts.java
+++ app/src/main/java/com/example/android/appcast/BuscarPodcasts.java
@@ -1,7 +1,10 @@
 package com.example.android.appcast;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class BuscarPodcasts extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class BuscarPodcasts extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buscar_podcasts);
+
+        TextView voltar = (TextView) findViewById(R.id.buscarPCVoltar);
+
+        voltar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent voltarIntent = new Intent(BuscarPodcasts.this, MainActivity.class);
+                startActivity(voltarIntent);
+
+            }
+        });
     }
 }
